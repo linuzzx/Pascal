f99f8b397c72a22c337091788380655f913658ee
EinarKL <onlineeinar@gmail.com>
2021-06-01 08:37:54 +0200

    Fikset fading_v2

--- Bachelor/index.js
+++ Bachelor/index.js
@@ -1,4 +1,10 @@
 $(function () {
-    $("html").fadeIn("slow");
-    $("body").fadeIn("slow");
+    $("html").fadeIn(2000);
+    $("header").fadeIn(2000);
+    $("#forsideLink").fadeIn(2000);
+    $(".thStatusrapport").fadeIn(2500);
+    $(".thProsjektskisse").fadeIn(3000);
+    $(".thForprosjektrapport").fadeIn(3500);
+    $(".thSluttrapport").fadeIn(4000);
+    $(".thPresentasjon").fadeIn(4500);
 });
