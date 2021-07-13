72222f661f8db5bd7710043486a2069c725da296
EinarKL <onlineeinar@gmail.com>
2021-07-13 22:23:51 +0200

    Lagt til BLD-oppsett

---
+++ Kubing/BLD/index.js
@@ -0,0 +1,17 @@
+$(function () {
+
+});
+
+function showCorners() {
+    $("#cornerMenu").css("display", "grid");
+    $("#cornerContent").css("display", "block");
+    $("#edgeMenu").css("display", "none");
+    $("#edgeContent").css("display", "none");
+}
+
+function showEdges() {
+    $("#cornerMenu").css("display", "none");
+    $("#cornerContent").css("display", "none");
+    $("#edgeMenu").css("display", "grid");
+    $("#edgeContent").css("display", "block");
+}
