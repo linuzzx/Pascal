ac52974ecb1e538ce73a7862bec2467bbbacadd8
EinarKL <onlineeinar@gmail.com>
2021-07-17 01:17:02 +0200

    Farge lagres

--- Kubing/BLD/index.js
+++ Kubing/BLD/index.js
@@ -147,24 +147,135 @@ function updateLetters() {
     }
 }
 
-function selectTopColor(button, color) {
-    selectedTopBtn = button;
-    selectedTopColor = color;
+function oppositeColor(color) {
+    switch (color) {
+        case 'white':
+            return 'yellow';
+        case 'yellow':
+            return 'white';
+        case 'green':
+            return 'blue';
+        case 'blue':
+            return 'green';
+        case 'red':
+            return 'orange';
+        case 'orange':
+            return 'red';
+    }
+}
 
-    localStorage.setItem("selectedTopBtn", selectedTopBtn);
+function cleanColor() {
+    selectedTopColor = "white";
+    selectedFrontColor = "green";
+    localStorage.setItem("selectedTopBtn", "btnTopW");
     localStorage.setItem("selectedTopColor", selectedTopColor);
+    localStorage.setItem("selectedFrontBtn", "btnFrontG");
+    localStorage.setItem("selectedFrontColor", selectedFrontColor);
 
     $(".topBtn").css("box-shadow", "none");
-    $("#"+button).css("box-shadow", "0 0 5px 1px "+color);
+    $("#btnTopW").css("box-shadow", "0 0 5px 1px white");
+
+    $(".frontBtn").css("box-shadow", "none");
+    $("#btnFrontG").css("box-shadow", "0 0 5px 1px green");
+
+    updateColor();
+}
+
+function selectTopColor(button, color) {
+    if (selectedFrontColor !== color && selectedFrontColor !== oppositeColor(color)) {
+        selectedTopBtn = button;
+        selectedTopColor = color;
+
+        localStorage.setItem("selectedTopBtn", selectedTopBtn);
+        localStorage.setItem("selectedTopColor", selectedTopColor);
+
+        $(".topBtn").css("box-shadow", "none");
+        $("#"+button).css("box-shadow", "0 0 5px 1px "+color);
+
+        updateColor();
+    }
+    else {
+        cleanColor();
+    }
 }
 
 function selectFrontColor(button, color) {
-    selectedFrontBtn = button;
-    selectedFrontColor = color;
+    if (selectedTopColor !== color && selectedTopColor !== oppositeColor(color)) {
+        selectedFrontBtn = button;
+        selectedFrontColor = color;
 
-    localStorage.setItem("selectedFrontBtn", selectedFrontBtn);
-    localStorage.setItem("selectedFrontColor", selectedFrontColor);
+        localStorage.setItem("selectedFrontBtn", selectedFrontBtn);
+        localStorage.setItem("selectedFrontColor", selectedFrontColor);
 
-    $(".frontBtn").css("box-shadow", "none");
-    $("#"+button).css("box-shadow", "0 0 5px 1px "+color);
+        $(".frontBtn").css("box-shadow", "none");
+        $("#" + button).css("box-shadow", "0 0 5px 1px " + color);
+
+        updateColor();
+    }
+    else {
+        cleanColor();
+    }
+}
+
+function updateColor() {
+
+    let colorArr = [];
+
+    let w = 'white';
+    let o = 'orange';
+    let g = 'green';
+    let r = 'red';
+    let b = 'blue';
+    let y = 'yellow';
+
+    switch (selectedTopColor) {
+        case "white":
+            colorArr = [w,o,g,r,b,y];
+            break;
+        case "yellow":
+            colorArr = [y,r,g,o,b,w];
+            break;
+        case "orange":
+            colorArr = [o,y,g,w,b,r];
+            break;
+        case "green":
+            colorArr = [g,o,y,r,w,b];
+            break;
+        case "red":
+            colorArr = [r,w,g,y,b,o];
+            break;
+        case "blue":
+            colorArr = [b,o,w,r,y,g];
+            break;
+    }
+
+    let cU = colorArr[0];
+    let cL = colorArr[1];
+    let cF = colorArr[2];
+    let cR = colorArr[3];
+    let cB = colorArr[4];
+    let cD = colorArr[5];
+
+    let nU = cU;
+    let nL = cL;
+    let nF = cF;
+    let nR = cR;
+    let nB = cB;
+    let nD = cD;
+
+    while (colorArr[2] !== selectedFrontColor) {
+        nL = colorArr[2];
+        nF = colorArr[3];
+        nR = colorArr[4];
+        nB = colorArr[1];
+
+        colorArr = [nU,nL,nF,nR,nB,nD];
+    }
+
+    $("#buttonsU :button").css("background", colorArr[0]);
+    $("#buttonsL :button").css("background", colorArr[1]);
+    $("#buttonsF :button").css("background", colorArr[2]);
+    $("#buttonsR :button").css("background", colorArr[3]);
+    $("#buttonsB :button").css("background", colorArr[4]);
+    $("#buttonsD :button").css("background", colorArr[5]);
 }
