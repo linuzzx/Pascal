096261d8b5947718431425a51a1bc55b0de9b646
EinarKL <onlineeinar@gmail.com>
2021-08-13 16:24:49 +0200

    Kan laste opp bilde i pixelart

--- PixelArt/index.js
+++ PixelArt/index.js
@@ -56,6 +56,35 @@ function closeNewDrawing() {
     showingNewDrawingBox = false;
 }
 
+function openDrawing(files) {
+    const image = document.getElementById("fileImage");
+    image.src = URL.createObjectURL(event.target.files[0]);
+
+    setTimeout(function() {
+        makeBoard(image.width, image.height);
+
+        var canvas = document.createElement('canvas');
+        canvas.width = image.width;
+        canvas.height = image.height;
+
+        var context = canvas.getContext('2d');
+        context.drawImage(image, 0, 0);
+
+        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+        console.log(imageData)
+        for (let i=0; i<image.height; i++) {
+            for (let j=0; j<image.width; j++) {
+                const index = (i*imageData.width + j) * 4;
+                const red = imageData.data[index];
+                const green = imageData.data[index + 1];
+                const blue = imageData.data[index + 2];
+                const alpha = imageData.data[index + 3];
+                $("#x"+j+"y"+i).css("background", 'rgba('+red+','+green+','+blue+','+alpha+')');
+            }
+        }
+    }, 500);
+}
+
 function saveDrawing() {
     const canvasDrawing = document.createElement("canvas");
     canvasDrawing.setAttribute("width", drawingWidth);
