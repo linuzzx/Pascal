5663ff9bad6a95c8107d8ce885d71347e0a0e6a3
EinarKL <onlineeinar@gmail.com>
2021-09-03 13:43:48 +0200

    Lagt til invers for dobbelttrekk

--- Kubing/CubeAnalyser/drawCube.js
+++ Kubing/CubeAnalyser/drawCube.js
@@ -71,6 +71,7 @@ function drawCube(allMoves) {
                 _r();
                 break;
             case "R2":
+            case "R2'":
                 _r2();
                 break;
             case "R'":
@@ -80,6 +81,7 @@ function drawCube(allMoves) {
                 _l();
                 break;
             case "L2":
+            case "L2'":
                 _l2();
                 break;
             case "L'":
@@ -89,6 +91,7 @@ function drawCube(allMoves) {
                 _f();
                 break;
             case "F2":
+            case "F2'":
                 _f2();
                 break;
             case "F'":
@@ -98,6 +101,7 @@ function drawCube(allMoves) {
                 _b();
                 break;
             case "B2":
+            case "B2'":
                 _b2();
                 break;
             case "B'":
@@ -107,6 +111,7 @@ function drawCube(allMoves) {
                 _u();
                 break;
             case "U2":
+            case "U2'":
                 _u2();
                 break;
             case "U'":
@@ -116,6 +121,7 @@ function drawCube(allMoves) {
                 _d();
                 break;
             case "D2":
+            case "D2'":
                 _d2();
                 break;
             case "D'":
@@ -125,6 +131,7 @@ function drawCube(allMoves) {
                 _x();
                 break;
             case "x2":
+            case "x2'":
                 _x2();
                 break;
             case "x'":
@@ -134,6 +141,7 @@ function drawCube(allMoves) {
                 _y();
                 break;
             case "y2":
+            case "y2'":
                 _y2();
                 break;
             case "y'":
@@ -143,6 +151,7 @@ function drawCube(allMoves) {
                 _z();
                 break;
             case "z2":
+            case "z2'":
                 _z2();
                 break;
             case "z'":
@@ -152,6 +161,7 @@ function drawCube(allMoves) {
                 _m();
                 break;
             case "M2":
+            case "M2'":
                 _m2();
                 break;
             case "M'":
@@ -161,6 +171,7 @@ function drawCube(allMoves) {
                 _s();
                 break;
             case "S2":
+            case "S2'":
                 _s2();
                 break;
             case "S'":
@@ -170,6 +181,7 @@ function drawCube(allMoves) {
                 _e();
                 break;
             case "E2":
+            case "E2'":
                 _e2();
                 break;
             case "E'":
@@ -179,6 +191,7 @@ function drawCube(allMoves) {
                 _uw();
                 break;
             case "Uw2":
+            case "Uw2'":
                 _uw2();
                 break;
             case "Uw'":
@@ -188,6 +201,7 @@ function drawCube(allMoves) {
                 _dw();
                 break;
             case "Dw2":
+            case "Dw2'":
                 _dw2();
                 break;
             case "Dw'":
@@ -197,6 +211,7 @@ function drawCube(allMoves) {
                 _fw();
                 break;
             case "Fw2":
+            case "Fw2'":
                 _fw2();
                 break;
             case "Fw'":
@@ -206,6 +221,7 @@ function drawCube(allMoves) {
                 _bw();
                 break;
             case "Bw2":
+            case "Bw2'":
                 _bw2();
                 break;
             case "Bw'":
@@ -215,6 +231,7 @@ function drawCube(allMoves) {
                 _rw();
                 break;
             case "Rw2":
+            case "Rw2'":
                 _rw2();
                 break;
             case "Rw'":
@@ -224,6 +241,7 @@ function drawCube(allMoves) {
                 _lw();
                 break;
             case "Lw2":
+            case "Lw2'":
                 _lw2();
                 break;
             case "Lw'":
