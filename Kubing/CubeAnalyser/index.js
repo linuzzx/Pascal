b336668c74ad923bfc188e0db16b699942df92ac
EinarKL <onlineeinar@gmail.com>
2021-09-04 13:25:20 +0200

    URL funker

--- Kubing/CubeAnalyser/index.js
+++ Kubing/CubeAnalyser/index.js
@@ -1,4 +1,5 @@
 $(function() {
+    getParams();
     listCubeTypes();
     drawCube("");
     updateTPS();
@@ -14,6 +15,31 @@ let url = "Kubing/CubeAnalyser/?";
 const cubeTypes = ["3x3", "2x2", "4x4", "Square-1"]; //["3x3", "2x2", "4x4", "5x5", "6x6", "7x7", "Clock", "Megaminx","Pyraminx", "Skewb", "Square-1"];
 let setupArray = [];
 let movesArray = [];
+
+function getParams() {
+    if (window.location.search !== "") {
+        const params = (window.location.search).split("?")[1].split("&");
+
+        for (let p of params) {
+            switch (p.split("=")[0]) {
+                case "setup":
+                    $("#taSetup").val(encodeURL(p.split("=")[1]));
+                    break;
+                case "moves":
+                    $("#taMoves").val(encodeURL(p.split("=")[1]));
+                    break;
+                case "time":
+                    $("#inputTime").val(encodeURL(p.split("=")[1]));
+                    break;
+            }
+        }
+        console.log(params);
+    }
+}
+
+function encodeURL(param) {
+    return param.replaceAll("%20", " ").replaceAll("%27", "'").replaceAll("%2F", "/").replaceAll("%0A", "\n").replaceAll("%26", "&").replaceAll("%3D", "=");
+}
 
 function listCubeTypes() {
     for (let cube of cubeTypes) {
@@ -33,8 +59,6 @@ function listCubeTypes() {
 function updateArrays() {
     const setup = getMoves("#taSetup");
     const moves = getMoves("#taMoves");
-
-    updateURL();
 
     drawCube(setup+" "+moves);
 
@@ -80,25 +104,40 @@ function updateTPS() {
 function updateURL() {
     const rawSetup = $("#taSetup").val();
     const rawMoves = $("#taMoves").val();
-    let urlExtra = "";
+    const rawTime = $("#inputTime").val();
+    let urlExtra = "?";
 
     // Må definere mellomrom, ', //, og ny linje
-    const space = "_";
-    const inverse = "-";
+    const space = "%20";
+    const inverse = "%27";
     const slash = "%2F";
     const newLine = "%0A";
+    const and = "%26";
+
+    rawSetup.replaceAll(" ", "%20").replaceAll("'", "%27").replaceAll("/", "%2F").replaceAll("\n", "%0A").replaceAll("&", "%26").replaceAll("=", "%3D");
+    rawMoves.replaceAll(" ", "%20").replaceAll("'", "%27").replaceAll("/", "%2F").replaceAll("\n", "%0A").replaceAll("&", "%26").replaceAll("=", "%3D");
+
     if (rawSetup !== "") {
         urlExtra += "setup="+rawSetup;
         if (rawMoves !== "") {
             urlExtra += "&moves="+rawMoves;
         }
+        if (rawTime !== "") {
+            urlExtra += "&time="+rawTime;
+        }
     }
     else if (rawMoves !== "") {
         urlExtra += "moves="+rawMoves;
+        if (rawTime !== "") {
+            urlExtra += "&time="+rawTime;
+        }
     }
-    const state = { 'page_id': 1, 'user_id': 5 }
-    const title = ''
+    else if (rawTime !== "") {
+        urlExtra += "time="+rawTime;
+    }
 
-    history.pushState(state, title, url+urlExtra);
-    console.log(url+urlExtra);
+    const state = {};
+    const title = "";
+
+    history.pushState(state, title, urlExtra);
 }
