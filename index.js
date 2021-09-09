62335eb3acec0bc307aaf447da81963860c6c5bf
EinarKL <onlineeinar@gmail.com>
2021-09-09 15:52:18 +0200

    Endringer i oppsett

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-function showMe() {
-    $("#divMe").css("display", "block");
-    $("#divMyProjects").css("display", "none");
-    $("#divOther").css("display", "none");
+let tabContents = [];
+
+$(function() {
+    tabContents = $(".tabContent");
+
+    adjustImg();
+    showHome();
+});
+
+function hideAll() {
+    for (let c of tabContents) {
+        $(c).css("display", "none");
+    }
 }
 
-function showMyProjects() {
-    $("#divMe").css("display", "none");
-    $("#divMyProjects").css("display", "block");
-    $("#divOther").css("display", "none");
+function showHome() {
+    hideAll();
+    $("#tabHome").css("display", "block");
 }
 
-function showOther() {
-    $("#divMe").css("display", "none");
-    $("#divMyProjects").css("display", "none");
-    $("#divOther").css("display", "block");
+function showProjects() {
+    hideAll();
+    $("#tabProjects").css("display", "block");
+}
+
+function showApps() {
+    hideAll();
+    $("#tabApps").css("display", "block");
+}
+
+function adjustImg() {
+    const size = $("h1").css("font-size");
+    
+    $(".tabAppsImg").css("height", size);
+    $(".tabAppsImg").css("width", "auto");
+    $(".tabAppsImg").css("margin-right", "0.25%");
 }
