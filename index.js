126945544f513f7c46c8074c0d031d87a899470f
EinarKL <onlineeinar@gmail.com>
2021-09-10 20:16:20 +0200

    Underline

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 let tabContents = [];
+let currentTab = 0;
 
 $(function() {
     tabContents = $(".tabContent");
 
     adjustImg();
+    makeUnderline();
     showHome();
-
-    const tabHomeLetters = $("#tabHome h1").text().split("");
-    let newTabHome = "";
-    for (let e of tabHomeLetters) {
-        newTabHome += "<einar>"+e+"</einar>"
-    }
-    $("#tabHome h1").html(newTabHome);
+    makeSingleLetters();
+        
+    $(window).resize(function(){
+        updateUnderline(currentTab);
+    });
 });
+
 
 function hideAll() {
     for (let c of tabContents) {
@@ -21,18 +22,45 @@ function hideAll() {
 }
 
 function showHome() {
+    currentTab = 0;
     hideAll();
     $("#tabHome").css("display", "block");
+    $("#underline").css("margin-left", $(window).width() / 9);
+    $("#underline").css("transition", "margin-left 0.1s linear");
+    $("#underline").css("-webkit-transition", "margin-left 0.1s linear");
 }
 
 function showProjects() {
+    currentTab = 1;
     hideAll();
     $("#tabProjects").css("display", "block");
+    $("#underline").css("margin-left", ($(window).width() / 9)*4);
+    $("#underline").css("transition", "margin-left 0.1s linear");
+    $("#underline").css("-webkit-transition", "margin-left 0.1s linear");
 }
 
 function showApps() {
+    currentTab = 2;
     hideAll();
     $("#tabApps").css("display", "block");
+    $("#underline").css("margin-left", ($(window).width() / 9)*7);
+    $("#underline").css("transition", "margin-left 0.1s linear");
+    $("#underline").css("-webkit-transition", "margin-left 0.1s linear");
+}
+
+function makeUnderline() {
+    $("#underline").css("height", "5px");
+    $("#underline").css("width", $(window).width() / 9);
+    $("#underline").css("margin-left", $(window).width() / 9);
+}
+
+function makeSingleLetters() {
+    const tabHomeLetters = $("#tabHome h1").text().split("");
+    let newTabHome = "";
+    for (let e of tabHomeLetters) {
+        newTabHome += "<einar>"+e+"</einar>"
+    }
+    $("#tabHome h1").html(newTabHome);
 }
 
 function adjustImg() {
@@ -41,4 +69,18 @@ function adjustImg() {
     $(".tabAppsImg").css("height", size);
     $(".tabAppsImg").css("width", "auto");
     $(".tabAppsImg").css("margin-right", "0.25%");
+}
+
+function updateUnderline(tab) {
+    console.log($(window).width());
+    $("#underline").css("width", $(window).width() / 9);
+    if (tab === 0) {
+        $("#underline").css("margin-left", $(window).width() / 9);
+    }
+    else if (tab === 1) {
+        $("#underline").css("margin-left", ($(window).width() / 9)*4);
+    }
+    else if (tab === 2) {
+        $("#underline").css("margin-left", ($(window).width() / 9)*7);
+    }
 }
