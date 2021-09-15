209d276e86e13bca70500bf542391af08b2bb9ea
EinarKL <onlineeinar@gmail.com>
2021-09-15 12:49:54 +0200

    Lagt til PR

--- Kubing/PR/index.js
+++ Kubing/PR/index.js
@@ -1,18 +1,46 @@
-const URL = "https://www.worldcubeassociation.org/persons/2015LUND03";
-//const URL = "./data.html";
+//const URL = "https://www.worldcubeassociation.org/persons/2015LUND03";
+const URL = "./data.html";
+let events = ["3x3x3","2x2x2","4x4x4","5x5x5","6x6x6","7x7x7","3x3x3 Blindfolded","3x3x3 Fewest Moves","3x3x3 One-Handed","Clock","Megaminx","Pyraminx","Skewb","Square-1","3x3x3 Multi-Blind"];
 
 $(function() {
     $.ajax({
         headers: "Access-Control-Allow-Origin",
         type: 'GET',
         url: URL,
-        responseType: 'jsonp',
         crossDomain: true,
         beforeSend: function(xhr){
             xhr.withCredentials = true;
     },
         success: function(data, textStatus, request){
-            console.log(data);
+            $("#hiddenDiv").load(URL + " tbody");
+            const out = $("#hiddenDiv")[0];
+
+            setTimeout(function(){
+                makePRList();
+            }, 500);
+            
         }
     });
 });
+
+function makePRList() {
+    const table = $("#tbodyPR");
+    let out = "<tr><th>Event</th><th>NR</th><th>CR</th><th>WR</th><th>Single</th><th>Average</th><th>WR</th><th>CR</th><th>NR</th></tr>";
+    let i = 1;
+    for (let event of events) {
+        out += "<tr>"+
+        "<td>"+$("#hiddenDiv > tbody:nth-child(3) > tr:nth-child("+i+") > td:nth-child(1)").text()+"</td>"+
+        "<td>"+$("#hiddenDiv > tbody:nth-child(3) > tr:nth-child("+i+") > td:nth-child(2)").text()+"</td>"+
+        "<td>"+$("#hiddenDiv > tbody:nth-child(3) > tr:nth-child("+i+") > td:nth-child(3)").text()+"</td>"+
+        "<td>"+$("#hiddenDiv > tbody:nth-child(3) > tr:nth-child("+i+") > td:nth-child(4)").text()+"</td>"+
+        "<th>"+$("#hiddenDiv > tbody:nth-child(3) > tr:nth-child("+i+") > td:nth-child(5)").text()+"</th>"+
+        "<th>"+$("#hiddenDiv > tbody:nth-child(3) > tr:nth-child("+i+") > td:nth-child(6)").text()+"</th>"+
+        "<th>"+$("#hiddenDiv > tbody:nth-child(3) > tr:nth-child("+i+") > td:nth-child(7)").text()+"</th>"+
+        "<td>"+$("#hiddenDiv > tbody:nth-child(3) > tr:nth-child("+i+") > td:nth-child(8)").text()+"</td>"+
+        "<td>"+$("#hiddenDiv > tbody:nth-child(3) > tr:nth-child("+i+") > td:nth-child(9)").text()+"</td>"+
+        "</tr>";
+        i++;
+    }
+
+    $(table).html(out);
+}
