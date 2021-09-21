1d94be650440debe3b2f57ce061c9e33f78a82f8
EinarKL <onlineeinar@gmail.com>
2021-09-21 16:33:33 +0200

    Begynt på bluetooth

---
+++ Bluetooth/index.js
@@ -0,0 +1,36 @@
+$(function() {
+    
+})
+
+function searchForBTDevice() {
+    console.log('Requesting Bluetooth Device...');
+    navigator.bluetooth.requestDevice(
+        {filters: [{services: ['battery_service']}]})
+    /*navigator.bluetooth.requestDevice({
+        acceptAllDevices: true,
+        optionalServices: ['battery_service'] // Required to access service later.
+        })*/
+    .then(device => {
+        console.log('Connecting to GATT Server...');
+        return device.gatt.connect();
+    })
+    .then(server => {
+        console.log('Getting Battery Service...');
+        return server.getPrimaryService('battery_service');
+    })
+    .then(service => {
+        console.log('Getting Battery Level Characteristic...');
+        return service.getCharacteristic('battery_level');
+    })
+    .then(characteristic => {
+        console.log('Reading Battery Level...');
+        return characteristic.readValue();
+    })
+    .then(value => {
+        let batteryLevel = value.getUint8(0);
+        console.log('> Battery Level is ' + batteryLevel + '%');
+    })
+    .catch(error => {
+        console.log('Argh! ' + error);
+    });
+}
