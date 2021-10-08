8241021fe645606fa54450e44b04bc8d974ccd4c
EinarKL <onlineeinar@gmail.com>
2021-10-08 12:07:42 +0200

    Started on Memo

--- Kubing/BLD/Memo/index.js
+++ Kubing/BLD/Memo/index.js
@@ -1,8 +1,8 @@
-let letters = "abcdefghijklmnopqrstuvwxyz".split("");
+let letters = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
 let numOfCubes = 1;
 
 $(function() {
-    
+    getMemo();
 });
         
 $(window).resize(function(){
@@ -13,8 +13,26 @@ function getMemo() {
     let memo = "";
 
     for (let i=0; i<numOfCubes; i++) {
-        
+        for (let j=0; j<10; j++) {
+            memo += letters[Math.floor(Math.random() * letters.length)] + " ";
+        }
     }
+console.log(memo);
+    $("#memo").html(memo);
+}
 
-    console.log(memo);
+function showMemo() {
+    $("#memo").css("visibility","visible");
+}
+
+function hideMemo() {
+    $("#memo").css("visibility","hidden");
+}
+
+function checkMemo() {
+    const success = $("#inpMemo").val().toLowerCase() === $("#memo").text().toLowerCase();
+    $("#inpMemo").val("");
+    alert(success)
+    getMemo();
+    showMemo();
 }
