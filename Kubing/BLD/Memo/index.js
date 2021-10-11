bcf234b68bd3fca188ca372d66905b42bddb6468
EinarKL <onlineeinar@gmail.com>
2021-10-11 10:44:58 +0200

    Added timer and colors

--- Kubing/BLD/Memo/index.js
+++ Kubing/BLD/Memo/index.js
@@ -1,12 +1,14 @@
-let letters = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+let letters = "abcdefghijklmnopqrstuvwx".toUpperCase().split("");
 let numOfCubes = 1;
 const numOfLetters3BLD = 20;
 const numOfLetters4BLD = 46;
 const numOfLetters5BLD = 86;
+let first = false;
+let start = 0;
+let interval = null;
+let time = "00.00";
 
 $(function() {
-    getMemo();
-
     $("#inpMemo").on('keyup', function (e) {
         if (e.key === 'Enter' || e.keyCode === 13) {
             checkMemo();
@@ -32,6 +34,11 @@ function getMemo() {
     }
     
     $("#memo").html(memo);
+    $("#result").html("");
+
+    first = true;
+    startTimer();
+    $("#inpMemo").focus();
 }
 
 function showMemo() {
@@ -39,23 +46,52 @@ function showMemo() {
 }
 
 function hideMemo() {
-    $("#memo").css("visibility","hidden");
-    $("#result").html("");
+    if (first) {
+        first = false;
+        $("#memo").css("visibility","hidden");
+        $("#result").html("");
+    }
 }
 
 function checkMemo() {
     const success = $("#inpMemo").val().toLowerCase().trim().split(" ").join("") === $("#memo").text().toLowerCase().split(" ").join("");
-    $("#inpMemo").val("");
+    let out = "<div><br>";
+    let inpMemo = $("#inpMemo").val().toLowerCase().trim().split(" ").join("").split("");
+    let memo = $("#memo").text().toLowerCase().trim().split(" ").join("").split("");
+
+    while (memo.length < inpMemo.length) {
+        memo.push("");
+    }
+
+    for (let i=0; i<memo.length; i++) {
+        if (inpMemo[i]) {
+            if (inpMemo[i] === memo[i]) {
+                out += "<e style='color: green'>"+inpMemo[i].toUpperCase()+"</e>";
+            }
+            else {
+                out += "<e style='color: red'>"+inpMemo[i].toUpperCase()+"</e>";
+            }
+        }
+        else {
+            out += "<e>"+memo[i].toUpperCase()+"</e>";
+        }
+    }
+
+    out += "</div>";
+
     if (success) {
-        $("#result").html("Success");
-        $("#result").css("color","green");
+        out += "<br><p style='color: green'>"+getTime(time)+"</p>";
     }
     else {
-        $("#result").html("DNF<br>"+$("#memo").html());
-        $("#result").css("color","red");
+        out += "<br><p style='color: red'>DNF<br>("+getTime(time)+")</p>";
     }
+
+    $("#result").html(out);
+
+    $("#inpMemo").val("");
     
-    getMemo();
+    $("#memo").html("<button class='btn btn-secondary' onclick='getMemo()'>Start</button>");
+
     showMemo();
 }
 
@@ -66,4 +102,55 @@ function adjustSize() {
     else {
         $("#inpMemo").css("width", "80%");
     }
+}
+
+function startTimer() {
+    start = new Date().getTime();
+    interval = setInterval(function() {
+        time = new Date().getTime() - start;
+    }, 10);
+}
+
+function stopTimer() {
+    clearInterval(interval);
+}
+
+function getTime(t) {
+    let timeStr = "";
+    let cs = Math.floor((t % 1000) / 10);
+    let s = Math.floor((t / 1000) % 60);
+    let m = Math.floor((t / 60000) % 60);
+    let h = Math.floor((t / 3600000) % 24);
+
+    if (h !== 0) {
+        if (m < 10) {
+            m = "0" + m;
+        }
+        if (s < 10) {
+            s = "0" + s;
+        }
+        if (cs < 10) {
+            cs = "0" + cs;
+        }
+        timeStr = h + ":" + m + ":" + s + "." + cs;
+    }
+    else {
+        if (m !==0) {
+            if (s < 10) {
+                s = "0" + s;
+            }
+            if (cs < 10) {
+                cs = "0" + cs;
+            }
+            timeStr = m + ":" + s + "." + cs;
+        }
+        else {
+            if (cs < 10) {
+                cs = "0" + cs;
+            }
+            timeStr = s + "." + cs;
+        }
+    }
+
+    return timeStr;
 }
