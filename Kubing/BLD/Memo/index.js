19a3fe3982b44b7138f7f469d8cbaf6f75a2ffcd
EinarKL <onlineeinar@gmail.com>
2021-10-11 14:30:10 +0200

    Minor fixes

--- Kubing/BLD/Memo/index.js
+++ Kubing/BLD/Memo/index.js
@@ -10,13 +10,6 @@ let time = "00.00";
 let cubeType = localStorage.getItem("cubeType") || "3BLD";
 let grouping = localStorage.getItem("grouping") || "1";
 
-let ubl="A",ub="A",ubr="B",ur="B",ufr="C",uf="C",ufl="D",ul="D",lub="E",lu="E",luf="F",lf="F",ldf="G",ld="G",ldb="H",lb="H",
-    ful="I",fu="I",fur="J",fr="J",fdr="K",fd="K",fdl="L",fl="L",ruf="M",ru="M",rub="N",rb="N",rdb="O",rd="O",rdf="P",rf="P",
-    bur="Q",bu="Q",bul="R",bl="R",bdl="S",bd="S",bdr="T",br="T",dfl="U",df="U",dfr="V",dr="V",dbr="W",db="W",dbl="X",dl="X";
-
-let letterScheme = [ubl,ub,ubr,ur,ufr,uf,ufl,ul,lub,lu,luf,lf,ldf,ld,ldb,lb,ful,fu,fur,fr,fdr,fd,fdl,fl,
-                    ruf,ru,rub,rb,rdb,rd,rdf,rf,bur,bu,bul,bl,bdl,bd,bdr,br,dfl,df,dfr,dr,dbr,db,dbl,dl];
-
 $(function() {
     $("#inpMemo").on('keyup', function (e) {
         if (e.key === 'Enter' || e.keyCode === 13) {
