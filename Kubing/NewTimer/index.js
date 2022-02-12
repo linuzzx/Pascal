f7c3d962534787835950f6acd6db308feb147d3f
EinarKL <onlineeinar@gmail.com>
2022-02-12 10:07:42 +0100

    The beginning of a new timer

---
+++ Kubing/NewTimer/index.js
@@ -0,0 +1,39 @@
+let audioContext;
+let analyser;
+let mic;
+
+$(function () {
+    
+});
+
+function initMic() {
+    audioContext = new AudioContext();
+    analyser = audioContext.createAnalyser();
+    if (navigator.mediaDevices) {
+        navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
+            mic = audioContext.createMediaStreamSource(stream);
+            console.log(stream);
+        }).catch((err) => {
+            console.log(err);
+            console.log("browser unable to access microphone\n(check to see if microphone is attached)");
+        });
+    } else {
+        console.log("browser unable to access media devices\n(update your browser)");
+    }
+}
+
+function readyTimer() {
+    $("#display").css("color", "yellow");
+}
+
+function startTimer() {
+    $("#display").css("color", "green");
+}
+
+function stopTimer() {
+    $("#display").css("color", "red");
+}
+
+function resetTimer() {
+    $("#display").css("color", "white");
+}
