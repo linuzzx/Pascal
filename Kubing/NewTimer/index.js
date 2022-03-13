4c34fc0f74612fac4dc94dd513018e763869e896
EinarKL <onlineeinar@gmail.com>
2022-03-13 22:43:03 +0100

    Fixed date

--- Kubing/NewTimer/index.js
+++ Kubing/NewTimer/index.js
@@ -732,8 +732,8 @@ function getDDMMYYYY(ms) {
     }
 
     const d = new Date(ms);
-    const day = d.getDay().toString().length < 2 ? "0" + d.getDay() : d.getDay();
-    const month = d.getMonth().toString().length < 2 ? "0" + d.getMonth() : d.getMonth();
+    const day = d.getDate().toString().length < 2 ? "0" + d.getDate() : d.getDate();
+    const month = (d.getMonth() + 1).toString().length < 2 ? "0" + (d.getMonth() + 1) : (d.getMonth() + 1);
     const year = d.getFullYear();
     const hours = d.getHours().toString().length < 2 ? "0" + d.getHours() : d.getHours();
     const minutes = d.getMinutes().toString().length < 2 ? "0" + d.getMinutes() : d.getMinutes();
