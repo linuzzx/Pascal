cb2dbb29c265b4b2c282e964bcd24e387601c06a
EinarKL <onlineeinar@gmail.com>
2022-05-09 15:51:31 +0200

    Init CFOP solver

---
+++ Kubing/Solver/3x3/CFOP/index.js
@@ -0,0 +1,39 @@
+$(() => {
+    draw333Svg('#svgCube', $("#inpScramble").val());
+
+    adjustSize();
+});
+
+function solveCube(scr) {
+    let w = new Worker("worker.js");
+    $("#solution").html("");
+    $("#searchDepth").html("");
+    
+    if (isValidScramble(scr)) {
+        w.postMessage(scr);
+        w.onmessage = e => {
+            if (e.data.length === 2) {
+                $("#solution").html("<h1>" + e.data[0] + "</h1>");
+            }
+            else if (e.data[0] === 0) {
+                $("#searchDepth").html("<h1><b>Time interuption</b></h1>");
+            }
+            else if (e.data[0] !== "Solved") {
+                $("#searchDepth").html("<h1>Searching at depth " + e.data[0] + "</h1>");
+            }
+            else {
+                $("#searchDepth").html("");
+            }
+        }
+    }
+}
+
+function isValidScramble(scr) {
+    return scr.trim() !== "";
+}
+
+function adjustSize() {
+    $("#svgCube").height(3 * $("#svgCube").width() / 4);
+    $("#inpScramble").css("font-size", "3vh");
+    $("#btnSolve").css("font-size", "3vh");
+}
