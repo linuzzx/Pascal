6147df0a1f436cc8586b8f7d78ecf787a0ac4037
EinarKL <onlineeinar@gmail.com>
2022-05-14 12:26:48 +0200

    Fixes

--- Kubing/KuberTilSalgs/script.js
+++ Kubing/KuberTilSalgs/script.js
@@ -5,14 +5,12 @@ $(() => {
         cubes = snapshot.val();
         
         for (let c of Object.values(cubes)) {
-            puzzles.push(new Puzzle(c.type, c.name, c.price, c.img));
+            puzzles.push(new Puzzle(c.type, c.name, c.price, c.img, c.info));
         }
 
         showPuzzles();
         adjustSize();
     });
-
-    adjustSize();
 });
 
 $(window).resize(() => {
@@ -20,21 +18,33 @@ $(window).resize(() => {
 });
 
 function showPuzzles() {
+    let id = 0;
     for (let p of puzzles) {
-        $("#puzzles").append("<puzzle><h1>"+p.name+"</h1><img src='"+p.image+"'><h1>"+p.price+"kr</h1></puzzle>");
+        $("#puzzles").append("<div class='puzzle'><div class='puzzle-content'>"+
+        "<div class='puzzle-front'><h2>"+p.name+"</h2><img src='"+p.image+"'><h2>"+p.price+"kr</h2></div>"+
+        "<div class='puzzle-back'><span>"+p.info+"</span></div>"+
+        "</div></div>");
+        id++;
     }
 }
 
 function adjustSize() {
-    $("puzzle").height($("body").width() / 3);
-    $("puzzle").width($("puzzle").height());
+    $(".puzzle").width($("body").width() / 5);
+    $(".puzzle").height($(".puzzle").width());
+
+    console.log(".puzzle",$("body").width() / 5);
+    console.log("img",$(".puzzle img").css("height"));
+    console.log("h2",$(".puzzle h2").height());
+
+    $(".puzzle img").height(($("body").width() / 5) - 2 * $(".puzzle h2").height());
 }
 
 class Puzzle {
-    constructor(type, name, price, image) {
+    constructor(type, name, price, image, info) {
         this.type = type;
         this.name = name;
         this.price = price;
         this.image = image;
+        this.info = info;
     }
 }
