fc64caa7cad8c3ed8fe9c20168ac030ce079837d
EinarKL <onlineeinar@gmail.com>
2022-07-18 08:37:10 +0200

    User inputs

--- Kubing/Solver/3x3/DR_new/index.js
+++ Kubing/Solver/3x3/DR_new/index.js
@@ -15,9 +15,15 @@ function solveCube(scr) {
         $("#inpScramble").prop('disabled', true);
         $("#btnSolve").prop('disabled', true);
         $("#btnScramble").prop('disabled', true);
-        w.postMessage(scr);
+        $("#inpEO").prop('disabled', true);
+        $("#inpDR").prop('disabled', true);
+        $("#inpHTR").prop('disabled', true);
+        w.postMessage([scr, $("#inpEO").val(), $("#inpDR").val(), $("#inpHTR").val()]);
         w.onmessage = e => {
-            if (e.data.length === 3) {
+            if (e.data.length === 2 && e.data[0] === "Step") {
+                $("#searchDepth").html("<h1>" + e.data[1] + "</h1>");
+            }
+            else if (e.data.length === 3) {
                 $("#solution").append("<h1>" + e.data[0] + "</h1>");
                 draw333Svg("#svg_" + step, e.data[2]);
                 scrollDown();
@@ -29,6 +35,9 @@ function solveCube(scr) {
                 $("#inpScramble").prop('disabled', false);
                 $("#btnSolve").prop('disabled', false);
                 $("#btnScramble").prop('disabled', false);
+                $("#inpEO").prop('disabled', false);
+                $("#inpDR").prop('disabled', false);
+                $("#inpHTR").prop('disabled', false);
             }
             else if (e.data[0] !== "Solved") {
                 $("#searchDepth").html("<h1>Searching at depth " + e.data[0] + "</h1>");
@@ -38,9 +47,31 @@ function solveCube(scr) {
                 $("#inpScramble").prop('disabled', false);
                 $("#btnSolve").prop('disabled', false);
                 $("#btnScramble").prop('disabled', false);
+                $("#inpEO").prop('disabled', false);
+                $("#inpDR").prop('disabled', false);
+                $("#inpHTR").prop('disabled', false);
             }
         }
     }
+}
+
+function changeSolValues() {
+    let eo = $("#inpEO");
+    let dr = $("#inpDR");
+    let htr = $("#inpHTR");
+    if ($(eo).val() === "") {
+        $(eo).val(1);
+    }
+    if ($(dr).val() === "") {
+        $(dr).val(1);
+    }
+    if ($(htr).val() === "") {
+        $(htr).val(1);
+    }
+
+    let solutions = $(eo).val() * $(dr).val() * $(htr).val();
+    
+    $("#totalSolutions").text(solutions + solutions === 1 ? solutions + " solution" : solutions + " solutions");
 }
 
 function isValidScramble(scr) {
