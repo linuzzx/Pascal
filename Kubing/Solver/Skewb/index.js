640384d45e65fdc7e881bb89e844561e4483b6eb
EinarKL <onlineeinar@gmail.com>
2022-09-19 17:14:12 +0200

    Started on skewb solver

---
+++ Kubing/Solver/Skewb/index.js
@@ -0,0 +1,63 @@
+$(() => {
+    drawSkewbSvg('#svgSkewb', $("#inpScramble").val());
+
+    adjustSize();
+});
+
+function solveCube(scr) {
+    let start = Date.now();
+    let w = new Worker("worker.js");
+    $("#solution").html("");
+    $("#searchDepth").html("");
+    let step = 0;
+    
+    if (isValidScramble(scr)) {
+        $("#inpScramble").prop('disabled', true);
+        $("#btnSolve").prop('disabled', true);
+        $("#btnScramble").prop('disabled', true);
+        w.postMessage([scr]);
+        w.onmessage = e => {
+            if (e.data.length === 2 && e.data[0] === "Step") {
+                $("#searchDepth").html("<h1>" + e.data[1] + "</h1>");
+            }
+            else if (e.data.length === 3) {
+                let sol = e.data[2][0];
+                sol !== "" ? $("#solution").append("<h1><b>Solution:</b> " + sol + "</h1><div><svg class='svgStep' id='svg_sol'></svg></div>") : "";
+                sol !== "" ? drawSkewbSvg("#svg_sol", [scr, sol].filter(s => {return s !== ""}).join(" ")) : "";
+                scrollDown();
+                adjustSize();
+                step++;
+            }
+            else if (e.data[0] === 0) {
+                $("#searchDepth").html("<h1><b>Time interuption</b></h1>");
+                $("#inpScramble").prop('disabled', false);
+                $("#btnSolve").prop('disabled', false);
+                $("#btnScramble").prop('disabled', false);
+            }
+            else if (e.data[0] !== "Solved") {
+                $("#searchDepth").html("<h1>Searching at depth " + e.data[0] + "</h1>");
+            }
+            else {
+                $("#searchDepth").html("<h1>" + (Date.now() - start) + " ms</h1>");
+                $("#inpScramble").prop('disabled', false);
+                $("#btnSolve").prop('disabled', false);
+                $("#btnScramble").prop('disabled', false);
+            }
+        }
+    }
+}
+
+function isValidScramble(scr) {
+    return scr.trim() !== "";
+}
+
+function scrollDown() {
+    const element = document.getElementsByTagName("body")[0].parentElement;
+    element.scrollTop = element.scrollHeight;
+}
+
+function adjustSize() {
+    $("svg").height(3 * $("#svgCube").width() / 4);
+    $("#inpScramble").css("font-size", "3vh");
+    $("button").css("font-size", "3vh");
+}
