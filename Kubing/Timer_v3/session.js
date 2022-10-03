d84db8f1cc4113fc7089d55f3c049db3069345f0
EinarKL <onlineeinar@gmail.com>
2022-10-03 14:11:13 +0200

    Deleted old versions of Timer

--- Kubing/Timer_v3/session.js
+++
@@ -1,6 +0,0 @@
-function Session(id, name, rank, scrType) {
-    this.id = id;
-    this.name = name;
-    this.rank = rank;
-    this.scrType = scrType;
-}
--- Kubing/Timer_v3/solution.js
+++
@@ -1,9 +0,0 @@
-function Solution(time, penalty, scramble, comment, date, finalTime, session) {
-    this.time = time;
-    this.penalty = penalty;
-    this.scramble = scramble;
-    this.comment = comment;
-    this.date = date;
-    this.finalTime = finalTime;
-    this.session = session;
-}
