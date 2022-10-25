a683b299f876e114f764a9c6e4beaad9cb610b1e
EinarKL <onlineeinar@gmail.com>
2022-10-25 15:22:57 +0200

    Fixes

--- Kubing/Solver/3x3/GeneralSolver/index.js
+++ Kubing/Solver/3x3/GeneralSolver/index.js
@@ -11,9 +11,7 @@ function solveCube(scramble) {
 
 function getSolution(solution) {
     console.log(solution);
-    if (solution === -1) {
-        $("#solution").html("<h1>Illegal end state</h1>");
-    }
+        $("#solution").html("<h1>" + solution + "</h1>");
 }
 
 function solve(scramble, callback, endState = "wwwwwwwwwooooooooogggggggggrrrrrrrrrbbbbbbbbbyyyyyyyyy") {
