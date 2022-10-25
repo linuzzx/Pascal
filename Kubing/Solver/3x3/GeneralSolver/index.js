435c60dd73f470eb3f3f2fac72b0bba32f9d4d9c
EinarKL <onlineeinar@gmail.com>
2022-10-25 16:21:32 +0200

    Fixes

--- Kubing/Solver/3x3/GeneralSolver/index.js
+++ Kubing/Solver/3x3/GeneralSolver/index.js
@@ -1,4 +1,5 @@
 let step = "0";
+let start;
 $(() => {
     drawScrambleNxN('#svgCube', 3, $("#inpScramble").val());
 
@@ -6,12 +7,21 @@ $(() => {
 });
 
 function solveCube(scramble) {
+    start = Date.now();
     solve(scramble, getSolution, $("#inpEndState").val());
 }
 
 function getSolution(solution) {
-    console.log(solution);
-        $("#solution").html("<h1>" + solution + "</h1>");
+    if (solution[0] === 1) {
+        let sol = removeRedundantMoves(solution[1]);
+        $("#solution").html("<h1><b>Solution:</b> " + sol + "</h1><h1><b>Moves:</b> " + sol.split(" ").length + "</h1><br>");
+        $("#searchDepth").html("<h1>" + (Date.now() - start) + " ms</h1>");
+    }
+    else if (solution[0] === 0) {
+        $("#solution").html("<h1>" + solution[1] + "</h1>");
+        $("#searchDepth").html("");
+    }
+    
 }
 
 function solve(scramble, callback, endState = "wwwwwwwwwooooooooogggggggggrrrrrrrrrbbbbbbbbbyyyyyyyyy") {
