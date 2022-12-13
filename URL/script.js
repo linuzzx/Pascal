7cecf07664010dded4dabea6cb4f60cfa055d723
EinarKL <onlineeinar@gmail.com>
2022-12-13 22:38:47 +0100

    Fixes

--- URL/script.js
+++ URL/script.js
@@ -8,11 +8,11 @@ console.log(shortURL);
             console.log(urls);
             let ind = Object.values(urls).findIndex(u => u.shortURL === shortURL);
 
-            if (ind) {
+            if (ind && Object.values(urls)[ind]) {
                 location.replace(Object.values(urls)[ind].longURL);
             }
             else {
-                location.replace("./noURL.html");
+                $("#noContent").css("display", "block");
             }
         });
     }
@@ -22,7 +22,16 @@ console.log(shortURL);
 });
 
 function shortenURL(url) {
-    
+    let shortenedURL = "";
+
+    while (shortenedURL === "") {
+        let short = "";
+
+        for (let i = 0; i < 5; i++) {
+            let r1 = Math.floor(Math.random() * 3);
+            let r2;
+        }
+    }
 }
 
 function copyURL(url) {
