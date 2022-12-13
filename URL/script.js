5fd7c3a5c155fede4f3885125007160e84305b39
EinarKL <onlineeinar@gmail.com>
2022-12-13 22:29:23 +0100

    Fixes

--- URL/script.js
+++ URL/script.js
@@ -6,17 +6,25 @@ console.log(shortURL);
         firebase.database().ref("URLs/").once("value", (snapshot) => {console.log(snapshot);
             let urls = snapshot.val();
             console.log(urls);
-            /* let ind = Object.values(urls).findIndex(u => u.shortURL === shortURL);
+            let ind = Object.values(urls).findIndex(u => u.shortURL === shortURL);
 
             if (ind) {
                 location.replace(Object.values(urls)[ind].longURL);
             }
             else {
                 location.replace("./noURL.html");
-            } */
+            }
         });
     }
     else {
         $("#content").css("display", "block");
     }
 });
+
+function shortenURL(url) {
+    
+}
+
+function copyURL(url) {
+    navigator.clipboard.writeText(url);
+}
