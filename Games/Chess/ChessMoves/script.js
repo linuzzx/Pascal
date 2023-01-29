f11c7cc32639af3704fe0efaaa63be9549de7958
EinarKL <onlineeinar@gmail.com>
2023-01-29 15:09:12 +0100

    Chess pieces

---
+++ Games/Chess/ChessMoves/script.js
@@ -0,0 +1,77 @@
+let currentMove = "";
+let prevMove = "";
+let tiles = [];
+let moves = [];
+
+$(() => {
+    init();
+});
+
+function init() {
+    adjustSize();
+    createSquares();
+    nextTile();
+}
+
+function createSquares() {
+    let size = $("#board").width() / 8;
+    for (let y = 0; y < 8; y++) {
+        let row = "<tr>";
+        for (let x = 0; x < 8; x++) {
+            let col = (y + x) % 2 === 0 ? "#F0D9B5" : "#B58863";
+            let dataCol = (y + x) % 2 === 0 ? "Light" : "Dark";
+            let id = ["a", "b", "c", "d", "e", "f", "g", "h"][x] + [8, 7, 6, 5, 4, 3, 2, 1][y];
+            let style = "background-color: " + col + "; width: " + size + "px; height: " + size + "px;";
+            let tile = "<td><div id='" + id + "' class='tiles' data-color='" + dataCol + "' style='" + style + "' onclick='checkTile(\"" + id + "\")'></div></td>";
+            row += tile;
+        }
+        row += "</tr>";
+        $("#board").append(row);
+    }
+    for (let t of $(".tiles")) {
+        tiles.push(t);
+    }
+}
+
+function checkTile(tile) {
+    if (currentMove === tile) {
+        for (let t of $(tiles)) {
+            $(t).removeClass("wrongTiles" + $(t).attr("data-color"));
+        }
+        nextTile();
+    }
+    else {console.log(tile);
+        $("#" + tile).addClass("wrongTiles" + $("#" + tile).attr("data-color"));console.log("wrongTiles" + $("#" + tile).attr("data-color"));
+    }
+}
+
+function nextTile() {
+    let rl = ["a", "b", "c", "d", "e", "f", "g", "h"][Math.floor(Math.random() * 8)];
+    let rn = Math.floor(Math.random() * 8) + 1;
+
+    currentMove = rl + rn;
+    
+    if (prevMove !== "") {
+        while (currentMove === prevMove) {
+            rl = ["a", "b", "c", "d", "e", "f", "g", "h"][Math.floor(Math.random() * 8)];
+            let rn = Math.floor(Math.random() * 8) + 1;
+
+            currentMove = rl + rn;
+        }
+    }
+
+    prevMove = currentMove;
+    $("#move").text(currentMove);
+}
+
+function adjustSize() {
+    let size;
+    if ($("body").width() >= $("body").height()) {
+        size = $("#content").height() * 8 / 10;
+    }
+    else {
+        size = $("#content").width() * 8 / 10;
+    }
+    $("#board").width(size);
+    $("#board").height(size);
+}
