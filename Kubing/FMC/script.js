0b3f90d21682972edfdf291c4c9a705e888c153e
einarkl <54262547+einarkl@users.noreply.github.com>
2023-04-26 15:55:07 +0200

    FMC

---
+++ Kubing/FMC/script.js
@@ -0,0 +1,88 @@
+let steps = {};
+
+$(() => {
+    initEvents();
+});
+
+function initEvents() {
+    $("#inpScramble").on("input", () => {
+        $("#inpInverseScramble").val(inverseAlg($("#inpScramble").val().trim()));
+    });
+
+    // $(".niss").on("click", e => {
+    $(".btnAddStep").on("click", e => {
+        addStep(e);
+    });
+
+    $(".niss input").on("click", e => {
+        e.stopPropagation();
+        console.log(e);
+    });
+}
+
+function deleteStep(n) {
+    for (let i = n; i < Object.keys(steps).length - 1; i++) {
+        steps[n] = steps[n + 1];
+        console.log(n);
+    }
+
+    delete steps[Object.keys(steps).length];
+
+    updateSteps();
+}
+
+function addStep(e) {
+    let n = Object.keys(steps).length + 1;
+    
+    steps[n] = {
+        // niss: e.currentTarget.dataset.niss,
+        niss: $(e.currentTarget).prev()[0].dataset.niss,
+        moves: "",
+        comment: ""
+    };
+
+    updateSteps();
+}
+
+function editSteps() {
+    for (let n of Object.keys(steps)) {
+        steps[n].moves = $("#inpStepsM" + n).val();
+        steps[n].comment = $("#inpStepsC" + n).val();
+    }
+
+    updateSolution();
+}
+
+function updateSteps() {
+    $("#normal").html("");
+    $("#inverse").html("");
+    for (let n of Object.keys(steps)) {
+        let step = "<div class='steps' id='step" + n +"'>" +
+                "<input id='inpStesN" + n + "' class='inpStepsN' type='number' placeholder='#' value='" + n + "' oninput='editSteps()'>" +
+                "<input id='inpStepsM" + n + "' class='inpStepsM' type='text' placeholder='Moves' value=\"" + steps[n].moves + "\" oninput='editSteps()'>" +
+                "<input id='inpStepsC" + n + "' class='inpStepsC' type='text' placeholder='Comment' value=\"" + steps[n].comment + "\" oninput='editSteps()'>" +
+                "<button id='btnDeleteStep" + n + "' class='btnDeleteStep' onclick='deleteStep(" + n + ")'>X</button>" +
+                "</div>";
+        $("#" + steps[n].niss).append(step);
+    }
+    
+    updateSolution();
+}
+
+function updateSolution() {
+    let normal = [];
+    let inverse = [];
+
+    for (let s of Object.keys(steps)) {
+        if (steps[s].niss === "normal") {
+            normal.push(steps[s].moves);
+        }
+        else {
+            inverse.push(steps[s].moves);
+        }
+    }
+
+    let solution = normal.join(" ") + inverseAlg(inverse.join(" "));
+
+    $("#solution").val("<h2>" + solution + " " + solution.split(" ").length + "(HTM)</h2>");
+}
